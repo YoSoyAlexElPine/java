29b341d785a313b0f9dbcf4f4d5862ffdbe9be44
YoSoyAlexElPine <machupichu0409@gmail.com>
2023-09-20 17:10:45 +0200

    hola mundo

---
+++ src/main/java/com/alex/trabajo/Trabajo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.alex.trabajo;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Trabajo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("");
+    }
+}
