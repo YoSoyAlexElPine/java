5b227f0ac0be65fcecd88e5c2960197b4d475588
YoSoyAlexElPine <machupichu0409@gmail.com>
2023-09-20 17:19:37 +0200

    hola mundo desde netbeans

--- src/main/java/com/alex/trabajo/Trabajo.java
+++ src/main/java/com/alex/trabajo/Trabajo.java
@@ -12,6 +12,13 @@ public class Trabajo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        
+        
+        
+        System.out.println(""
+                + ""
+                + "");
         System.out.println("");
     }
 }
