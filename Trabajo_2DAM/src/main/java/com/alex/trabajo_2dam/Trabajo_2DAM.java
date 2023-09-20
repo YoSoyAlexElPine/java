956c5e9fe5da5b467ba4bfbda271f0714ba7427e
YoSoyAlexElPine <machupichu0409@gmail.com>
2023-09-20 18:47:41 +0200

    hola mundo desde netbeans

---
+++ Trabajo_2DAM/src/main/java/com/alex/trabajo_2dam/Trabajo_2DAM.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.alex.trabajo_2dam;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Trabajo_2DAM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        System.out.println("");
+    }
+}
